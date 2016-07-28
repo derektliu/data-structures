32653927399b07ac524823029a9001b502486b31
derektliu <liu.derekt@gmail.com>
2016-07-28 15:06:49 -0700

    Completed hashTable insert and retrieve methods.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,10 +7,26 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (!this._storage[index]) {
+    // console.log('create new array');
+    this._storage[index] = [];
+  }
+
+  var newTuple = [k, v];
+  // console.log(newTuple);
+  this._storage[index].push(newTuple);
+  // console.log(this._storage[index]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
