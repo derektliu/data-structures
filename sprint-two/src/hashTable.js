599ba1f443f75dc44443b909115587cbed56d419
derektliu <liu.derekt@gmail.com>
2016-07-28 15:23:05 -0700

    Completed hashTable insert

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -12,11 +12,25 @@ HashTable.prototype.insert = function(k, v) {
     // console.log('create new array');
     this._storage[index] = [];
   }
-
-  var newTuple = [k, v];
-  // console.log(newTuple);
-  this._storage[index].push(newTuple);
-  // console.log(this._storage[index]);
+  // check if key exist in bucket if so reassign that key to new value
+  // if (this.retrieve(k) !== undefined) {
+  //   this._storage[index]
+  // } else {
+  //   var newTuple = [k, v];
+  //   this._storage[index].push(newTuple);
+  // }
+  var foundIndex = false;
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      foundIndex = true;
+      this._storage[index][i][1] = v;
+      break;
+    }
+  }
+  if (!foundIndex) {
+    var newTuple = [k, v];
+    this._storage[index].push(newTuple); 
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
