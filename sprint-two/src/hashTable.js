6af821efa8efbf285b95d06320d29e77ff76484f
derektliu <liu.derekt@gmail.com>
2016-07-29 11:35:28 -0700

    Completed insert and retrieve for Hash Tables.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,96 +1,58 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this._size = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  // if bucket list is <75% of storage length, insert value
-  // else, rehash table.
-
-  // Does bucket exist
-  if (!this._storage[index]) {
-    this._storage[index] = [];
+  // check if bucket exists
+  if (!this._storage.get(index)) {
+    // if not, create empty bucket array
+    this._storage.set(index, []);
   }
+  // grab bucket at index
+  var bucket = this._storage.get(index);
+  var found = false;
 
-  var foundIndex = false;
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      foundIndex = true;
-      this._storage[index][i][1] = v;
-      break;
+  // check for key at bucket
+  for (var i = 0; i < bucket.length; i++) {
+    // if key at current tuple equals k
+    if (bucket[i][0] === k) {
+    // replace value if key exists
+      this._storage.get(index)[1] = v;
+      found = true;
     }
   }
-  if (!foundIndex) {
-    var newTuple = [k, v];
-    this._storage[index].push(newTuple); 
-    this._size++;
+
+  // if key is not in bucket
+  if (!found) {
+    // push tuple with new key and value to bucket
+    this._storage.get(index).push([k, v]);
   }
-  // console.log(this._size);
-  // if bucket list is <75% of storage length, insert value
-  if (this._size >= this._limit * .75) {
-    // reassign limit to limit * 2
 
-    this._limit = this._limit * 2;
-
-    // create new limited array with new limit
-    var rehashed = LimitedArray(this._limit);
-    rehashed._storage = [];
-    
-
-    // loop through current storage
-    for (var i = 0; i < size; i++) {
-      console.log('in first for loop');
-      for (var j = 0; j < this._storage[i].length; j++) {
-      // insert each value to new array
-        console.log('in nested for loop');
-
-        var key = this._storage[i][j][0];
-        var hashedKey = getIndexBelowMaxForKey(key, this._limit);
-        var value = this._storage[i][j][1];
-
-        console.log('key', key);
-        console.log('hashedKey', hashedKey);
-        console.log('value', value);
-
-        // take key var rehash it
-        var tuple = [hashedKey, value];
-        console.log('tuple', tuple);
-        // got rehashed table set & push
-        rehashed[hashedKey].push(tuple);
-      }
-    }
-    // replace storage with new storage
-    this._storage = rehashed;
-  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      return this._storage[index][i][1];
+
+  // get bucket at index
+  var bucket = this._storage.get(index);
+
+  // iterate through bucket to check for 'k' key
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
     }
   }
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      this._storage[index].splice(i, 1);
-      this._size--;
-    }
-  }
-
 };
 
-
+// var test = new Hashtable();
 
 /*
  * Complexity: What is the time complexity of the above functions?
