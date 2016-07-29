510a509fdbd530653221195b496730f05d0733e8
derektliu <liu.derekt@gmail.com>
2016-07-28 20:04:39 -0700

    Completed binarySearchTree insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,38 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+
+  tree.value = value;
+
+  tree.left = undefined;
+  tree.right = undefined;
+
+  tree.insert = function(value, node) {
+
+    node = node || this;
+    console.log(node);
+
+    // if smaller value
+    if (value < node.value) {
+      if (node.left === undefined) {
+        node.left = BinarySearchTree(value);
+      } else {
+        node.insert(value, node.left);
+      }
+    } else if (value > node.value) { // if larger value
+      if (node.right === undefined) {
+        node.right = BinarySearchTree(value);
+      } else {
+        node.insert(value, node.right);
+      }
+    }
+
+
+
+  };
+  tree.contains = function() {};
+  tree.depthFirstLog = function() {};
+
+  return tree;
 };
 
 
