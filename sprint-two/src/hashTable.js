88f90f77fd7dde5c0c0572e6fe288b98ea8df2bf
Gamerlazer <gamerlazer@gmail.com>
2016-07-29 19:29:07 -0700

    Refactored HashTable insert

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,30 +6,26 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  // check if bucket exists
-  if (!this._storage.get(index)) {
-    // if not, create empty bucket array
-    this._storage.set(index, []);
-  }
-  // grab bucket at index
-  var bucket = this._storage.get(index);
-  var found = false;
+  var bucket = this._storage.get(index) || [];
+  console.log(bucket);
 
   // check for key at bucket
   for (var i = 0; i < bucket.length; i++) {
     // if key at current tuple equals k
     if (bucket[i][0] === k) {
     // replace value if key exists
-      this._storage.get(index)[i][1] = v;
-      found = true;
+      var oldValue = bucket[i][1];
+      bucket[i][1] = v;
+      return oldValue;
     }
   }
 
-  // if key is not in bucket
-  if (!found) {
-    // push tuple with new key and value to bucket
-    this._storage.get(index).push([k, v]);
-  }
+  bucket.push([k, v]);
+  this._storage.set(index, bucket);
+  // // if key is not in bucket
+  // if (!found) {
+  //   // push tuple with new key and value to bucket
+  // }
 
 };
 
